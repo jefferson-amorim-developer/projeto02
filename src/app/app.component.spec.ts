da02a0826d4bd73a5e446924d86fe31930317a46
JEFFERSON SILVA DE AMORIM <jefferson.amorim@trt8.jus.br>
2022-03-08 09:17:10 -0300

    Teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,15 +10,17 @@ describe('AppComponent', () => {
         }).compileComponents();
     });
 
-    it("should create the app", () => {
+    it('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
+
         expect(app).toBeTruthy();
     });
 
     it(`should have as title 'projeto02'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
+
         expect(app.title).toEqual('projeto02');
     });
 
@@ -26,6 +28,7 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
+
         expect(compiled.querySelector('.content span')?.textContent).toContain('projeto02 app is running!');
     });
 });
