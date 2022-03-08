88054b08a93be23294a9a85a4bf9e479ad994a08
JEFFERSON AMORIM <jefferson.amorim.developer@gmail.com>
2022-03-07 23:45:25 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'projeto02';
+    title = 'projeto02';
+
+    constructor() {}
+
+    hello(content: any): any {
+        console.log(`hello ${content}`);
+        console.log('world');
+        return content;
+    }
 }
